ea63bdd5eab370cb4ea9f971c21e34b78aaa360b
CJ Harmath <harmath@deshaw.com>
2019-02-21 13:00:26 -0500

    Added the properties necessary for xWebAppPoolDefaults to ensure that all app pool are always running or will be suspended instead of terminated.

--- DSCResources/MSFT_xWebAppPoolDefaults/MSFT_xWebAppPoolDefaults.psm1
+++ DSCResources/MSFT_xWebAppPoolDefaults/MSFT_xWebAppPoolDefaults.psm1
@@ -17,7 +17,7 @@ function Get-TargetResource
 {
     <#
     .SYNOPSIS
-        This will return a hashtable of results 
+        This will return a hashtable of results
     #>
 
     [CmdletBinding()]
@@ -37,6 +37,10 @@ function Get-TargetResource
     return @{
         ManagedRuntimeVersion = (Get-Value -Path '' -Name 'managedRuntimeVersion')
         IdentityType          = (Get-Value -Path 'processModel' -Name 'identityType')
+        StartMode             = (Get-Value -Path '' -Name 'startMode')
+        IdleTimeout           = (Get-Value -Path 'processModel' -Name 'idleTimeout')
+        IdleTimeoutAction     = (Get-Value -Path 'processModel' -Name 'idleTimeoutAction')
+        RestartTimeLimit      = (Get-Value -Path 'recycling/periodicRestart' -Name 'time')
     }
 }
 
@@ -45,6 +49,44 @@ function Set-TargetResource
     <#
     .SYNOPSIS
         This will set the desired state
+
+    .PARAMETER ApplyTo
+        Only Machine is supported.
+
+    .PARAMETER ManagedRuntimeVersion
+        Indicates the CLR version to be used by the application pool.
+        The values that are allowed for this property are: v4.0, v2.0, and ''.
+
+    .PARAMETER IdentityType
+        Indicates the account identity under which the application pool runs.
+        The values that are allowed for this property are:
+            ApplicationPoolIdentity, LocalService, LocalSystem, NetworkService, and SpecificUser.
+
+    .PARAMETER StartMode
+        Indicates the startup type for the application pool.
+        The values that are allowed for this property are: OnDemand, AlwaysRunning.
+
+    .PARAMETER IdleTimeout
+        Indicates the amount of time (in minutes) a worker process will remain idle before it shuts down.
+        The value must be a string representation of a TimeSpan value and
+        must be less than the restartTimeLimit property value.
+        The valid range (in minutes) is 0 to 43200.
+
+    .PARAMETER IdleTimeoutAction
+        Indicates the action to perform when the idle timeout duration has been reached.
+        The values that are allowed for this property are: Terminate, Suspend
+
+        A suspended worker process remains alive but is paged-out to disk, reducing the system resources it consumes.
+        When a user accesses the site again, the worker process wakes up from suspension and is quickly available.
+        When an idle worker process is terminated, the worker process is shut down,
+        and the startup period will be longer when the site is subsequently accessed.
+        Terminating the process is the default behavior.
+
+    .PARAMETER RestartTimeLimit
+        Indicates the period of time (in minutes) after which the application pool will recycle.
+        The value must be a string representation of a TimeSpan value.
+        The valid range (in minutes) is 0 to 432000.
+        A value of 0 means the application pool does not recycle on a regular interval.
     #>
 
     [CmdletBinding()]
@@ -64,13 +106,33 @@ function Set-TargetResource
         [Parameter()]
         [ValidateSet('ApplicationPoolIdentity','LocalService','LocalSystem','NetworkService')]
         [System.String]
-        $IdentityType
+        $IdentityType,
+
+        [ValidateSet('OnDemand', 'AlwaysRunning')]
+        [String] $StartMode,
+
+        [ValidateScript({
+            ([ValidateRange(0, 43200)]$valueInMinutes = [TimeSpan]::Parse($_).TotalMinutes); $?
+        })]
+        [String] $IdleTimeout,
+
+        [ValidateSet('Terminate', 'Suspend')]
+        [String] $IdleTimeoutAction,
+
+        [ValidateScript({
+            ([ValidateRange(0, 432000)]$valueInMinutes = [TimeSpan]::Parse($_).TotalMinutes); $?
+        })]
+        [String] $RestartTimeLimit
     )
 
     Assert-Module
 
     Set-Value -Path '' -Name 'managedRuntimeVersion' -NewValue $ManagedRuntimeVersion
     Set-Value -Path 'processModel' -Name 'identityType' -NewValue $IdentityType
+    Set-Value -Path '' -Name 'startMode' -NewValue $StartMode
+    Set-Value -Path 'processModel' -Name 'idleTimeout' -NewValue $IdleTimeout
+    Set-Value -Path 'processModel' -Name 'idleTimeoutAction' -NewValue $IdleTimeoutAction
+    Set-Value -Path 'recycling/periodicRestart' -Name 'time' -NewValue $RestartTimeLimit
 }
 
 function Test-TargetResource
@@ -79,8 +141,46 @@ function Test-TargetResource
     .SYNOPSIS
         This tests the desired state. If the state is not correct it will return $false.
         If the state is correct it will return $true
+
+    .PARAMETER ApplyTo
+        Only Machine is supported.
+
+    .PARAMETER ManagedRuntimeVersion
+        Indicates the CLR version to be used by the application pool.
+        The values that are allowed for this property are: v4.0, v2.0, and ''.
+
+    .PARAMETER IdentityType
+        Indicates the account identity under which the application pool runs.
+        The values that are allowed for this property are:
+            ApplicationPoolIdentity, LocalService, LocalSystem, NetworkService, and SpecificUser.
+
+    .PARAMETER StartMode
+        Indicates the startup type for the application pool.
+        The values that are allowed for this property are: OnDemand, AlwaysRunning.
+
+    .PARAMETER IdleTimeout
+        Indicates the amount of time (in minutes) a worker process will remain idle before it shuts down.
+        The value must be a string representation of a TimeSpan value and
+        must be less than the restartTimeLimit property value.
+        The valid range (in minutes) is 0 to 43200.
+
+    .PARAMETER IdleTimeoutAction
+        Indicates the action to perform when the idle timeout duration has been reached.
+        The values that are allowed for this property are: Terminate, Suspend
+
+        A suspended worker process remains alive but is paged-out to disk, reducing the system resources it consumes.
+        When a user accesses the site again, the worker process wakes up from suspension and is quickly available.
+        When an idle worker process is terminated, the worker process is shut down,
+        and the startup period will be longer when the site is subsequently accessed.
+        Terminating the process is the default behavior.
+
+    .PARAMETER RestartTimeLimit
+        Indicates the period of time (in minutes) after which the application pool will recycle.
+        The value must be a string representation of a TimeSpan value.
+        The valid range (in minutes) is 0 to 432000.
+        A value of 0 means the application pool does not recycle on a regular interval.
     #>
-    
+
     [CmdletBinding()]
     [OutputType([System.Boolean])]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSDSCUseVerboseMessageInDSCResource", "")]
@@ -99,7 +199,23 @@ function Test-TargetResource
         [Parameter()]
         [ValidateSet('ApplicationPoolIdentity','LocalService','LocalSystem','NetworkService')]
         [System.String]
-        $IdentityType
+        $IdentityType,
+
+        [ValidateSet('OnDemand', 'AlwaysRunning')]
+        [String] $StartMode,
+
+        [ValidateScript({
+            ([ValidateRange(0, 43200)]$valueInMinutes = [TimeSpan]::Parse($_).TotalMinutes); $?
+        })]
+        [String] $IdleTimeout,
+
+        [ValidateSet('Terminate', 'Suspend')]
+        [String] $IdleTimeoutAction,
+
+        [ValidateScript({
+            ([ValidateRange(0, 432000)]$valueInMinutes = [TimeSpan]::Parse($_).TotalMinutes); $?
+        })]
+        [String] $RestartTimeLimit
     )
 
     Assert-Module
@@ -107,7 +223,7 @@ function Test-TargetResource
     if (-not((Confirm-Value -Path '' `
                             -Name 'managedRuntimeVersion' `
                             -NewValue $ManagedRuntimeVersion)))
-    { 
+    {
         return $false
     }
 
@@ -115,9 +231,37 @@ function Test-TargetResource
                             -Name 'identityType' `
                             -NewValue $IdentityType)))
     {
-        return $false 
+        return $false
     }
-    
+
+    if (-not((Confirm-Value -Path '' `
+                            -Name 'startMode' `
+                            -NewValue $StartMode)))
+    {
+        return $false
+    }
+
+    if (-not((Confirm-Value -Path 'processModel' `
+                            -Name 'idleTimeout' `
+                            -NewValue $IdleTimeout)))
+    {
+        return $false
+    }
+
+    if (-not((Confirm-Value -Path 'processModel' `
+                            -Name 'idleTimeoutAction' `
+                            -NewValue $IdleTimeoutAction)))
+    {
+        return $false
+    }
+
+    if (-not((Confirm-Value -Path 'recycling/periodicRestart' `
+                            -Name 'time' `
+                            -NewValue $RestartTimeLimit)))
+    {
+        return $false
+    }
+
     return $true
 }
 
@@ -142,7 +286,7 @@ function Confirm-Value
         [System.String]
         $NewValue
     )
-    
+
     if (-not($NewValue))
     {
         # if no new value was specified, we assume this value is okay.
@@ -200,7 +344,7 @@ function Set-Value
             -Filter "system.applicationHost/applicationPools/applicationPoolDefaults$Path" `
             -Name $Name `
             -Value "$NewValue"
-        
+
         $relPath = $Path + '/' + $Name
         Write-Verbose($LocalizedData.SettingValue -f $relPath,$NewValue);
     }
